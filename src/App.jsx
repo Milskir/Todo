805bfe30c43b284e5cd40e99b18946c3e25f6c13
Milskir <mtissari24@students.oamk.fi>
2026-09-07 06:26:33 +0300

    Todo part 7 done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { useState,useEffect } from 'react'
 import axios from 'axios'
 import Row from './components/Row.jsx'
+import { useUser } from './context/useUser.js'
 
 const apiUrl = 'http://localhost:3001'
 
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
+  const { user } = useUser()
 
   useEffect(() => {
     axios.get(`${apiUrl}/tasks`)
@@ -21,8 +23,9 @@ function App() {
 
   const addTask = (e) => {
     e.preventDefault()
+    const headers = {headers: {Authorization: `Bearer ${user.token}`}}
     const newTask = { description: task }
-    axios.post(`${apiUrl}/tasks`, { task: newTask })
+    axios.post(`${apiUrl}/tasks`, { task: newTask },headers)
       .then(response => {
         setTasks(currentTasks => [...currentTasks, response.data])
         setTask('')
@@ -33,7 +36,8 @@ function App() {
   }
 
   const deleteTask = (deleted) => {
-    axios.delete(`${apiUrl}/tasks/${deleted}`)
+    const headers = {headers: {Authorization: `Bearer ${user.token}`}}
+    axios.delete(`${apiUrl}/tasks/${deleted}`,headers)
       .then(response => {
         setTasks(currentTasks => currentTasks.filter(item => item.id !== deleted))
       })
